f734249d08b7ec0d176fa4f505d19084fcf53592
LiJunpeng <lijunpeng91@gmail.com>
2017-09-07 17:13:53 -0700

    update some comments

--- public/js/controllers/urlController.js
+++ public/js/controllers/urlController.js
@@ -2,7 +2,7 @@ var app = angular.module("tinyurlApp");
 
 app.controller("urlController", 
     ["$scope", "$http", "$routeParams", function ($scope, $http, $routeParams) {
-    $http.get("/api/v1/urls/" + $routeParams.shortUrl)  // $routeParams??????? // "/api/v1/urls/" 不能少最后一个"/"
+    $http.get("/api/v1/urls/" + $routeParams.shortUrl)  // $routeParams = /:shortUrl // "/api/v1/urls/" 不能少最后一个"/"
         .success(function (data) {
             $scope.shortUrl = data.shorUrl;
             $scope.longUrl = data.longUrl;
