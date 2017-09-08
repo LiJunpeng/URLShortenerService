0ca9ccaed28bac57762172829df2769a80bc48c2
LiJunpeng <lijunpeng91@gmail.com>
2017-09-08 16:36:09 -0700

    update redirect.js

--- app/routes/redirect.js
+++ app/routes/redirect.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path");
 
 var urlService = require('../services/urlService');
+
 
 router.get('*', function (req, res) {   // * 指任何东西
     var shortUrl = req.originalUrl.slice(1); // 传入的url第一位是'\'，slice去掉第一位
     urlService.getLongUrl(shortUrl, function (url) {
         if (url) {
             res.redirect(url.longUrl);
+            statsService.logRequest(shortUrl, req);
         } else {
-            res.sendfile('./public/views/404.html');
+            res.sendFile("404.html", {root: path.join(__dirname, "../public/views/")});
         }
     });
 
