e30cdfdbd708528bf22952228493aa4db9f6b3f9
LiJunpeng <lijunpeng91@gmail.com>
2017-09-08 16:51:47 -0700

    update statsServicejs

--- app/server.js
+++ app/server.js
@@ -6,10 +6,13 @@ var redirectRouter = require('./routes/redirect');
 var indexRouter = require('./routes/index');
 var mongoose = require('mongoose');
 var dbLink = require('./DBaccess');
+var useragent = require("express-useragent");
 
 mongoose.connect(dbLink.dbLink);
 
 app.use('/public', express.static(__dirname + "/public")); 
+
+app.use(useragent.express);
 
 app.use('/api/v1', restRouter);
 
--- app/services/statsService.js
+++ app/services/statsService.js
@@ -2,6 +2,13 @@ var logRequest = function (shortUrl, req) {
     var reqInfo = {};
     reqInfo.shortUrl = shortUrl;
     reqInfo.referer = req.headers.referer || "Unknown";  // could be null
+    reqInfo.platform = req.useragent.platform || "Unknown";
+    reqInfo.browser = req.useragent.browser || "Unknown";
+    var ip = req.headers["x-forwarded-for"] ||
+            req.connection.remoteAddress ||
+            req.socket.remoteAddress || 
+            req.connection.socket.remoteAddress;
+    
 };
 
 
