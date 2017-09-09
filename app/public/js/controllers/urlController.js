6e7afccd71e821afa9f7d522969d0cb0c45a6cfd
LiJunpeng <lijunpeng91@gmail.com>
2017-09-08 20:36:28 -0700

    add time chart

--- app/public/js/controllers/urlController.js
+++ app/public/js/controllers/urlController.js
@@ -13,6 +13,8 @@ app.controller("urlController",
             $scope.totalClicks = data;
         });
 
+
+
     var renderChart = function (chart, infos) {
         $scope[chart + "Data"] = [];
         $scope[chart + "Labels"] = [];
@@ -29,6 +31,36 @@ app.controller("urlController",
     renderChart("pie", "country");
     renderChart("base", "platform");
     renderChart("bar", "browser");
+
+    $scope.getTime = function (time) {
+        $scope.lineLabels = [];
+        $scope.lineData = [];
+        $scope.time = time;
+        $http.get("/api/v1/urls/" + $routeParams.shortUrl + "/" + time)
+            .success(function (data) {
+                data.forEach(function (info) {
+
+                    var legend = '';
+                    if (time === 'hour') {
+                        if (info._id.minutes < 10) {
+                            info._id.minutes = '0' + info._id.minutes;
+                        }
+                        legend = info._id.hour + ':' + info._id.minutes;
+                    }
+                    if (time === 'day') {
+                        legend = info._id.hour + ':00';
+                    }
+                    if (time === 'month') {
+                        legend = info._id.month + '/' + info._id.day;
+                    }
+
+                    $scope['lineLabels'].push(legend);
+                    $scope['lineData'].push(info.count);
+                });
+            });
+    };
+
+    $scope.getTime('hour');
 
 }]);
 
